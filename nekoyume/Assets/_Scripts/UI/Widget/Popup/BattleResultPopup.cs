71b304cd3794e9a076c69993bed60a39777e460c
plafina2 <plafina2@gmail.com>
2021-11-03 12:42:07 +0900

    Fix stage balnce for 221~240

--- nekoyume/Assets/_Scripts/UI/Widget/Popup/BattleResultPopup.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Popup/BattleResultPopup.cs
@@ -259,6 +259,11 @@ namespace Nekoyume.UI
 
             UpdateView(isBoosted);
             HelpTooltip.HelpMe(100006, true);
+
+            if (!model.IsClear)
+            {
+                StartCoroutine(OnClickRepeat());
+            }
         }
 
         public override void Close(bool ignoreCloseAnimation = false)
